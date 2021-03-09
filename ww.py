6ba785d1c24b6b1abe00b775bb7653c84eb1715b
484669732 <80316502+484669732@users.noreply.github.com>
2021-03-09 17:52:06 +0800

    Update ww.py

--- ww.py
+++ ww.py
@@ -2,26 +2,26 @@ num1=1
 name1="a"
 jiage1=20
 pinzhi1="xx"
-lexing1="user帽子"
+leixing1="user帽子"
 xiaoliang1=1000
 
 num2=2
 name2="b"
 jiage2=20
 pinzhi2="xx"
-lexing2="scav背心"
+leixing2="scav背心"
 xiaoliang2=3000
 
 num3=3
 name3="c"
 jiage3=20
 pinzhi3="A"
-lexing3="雷朋眼镜"
+leixing3="雷朋眼镜"
 xiaoliang3=50000
 
 print("--------------欢迎来到衣服商城系统--------------")
 print("编号    衣服名称    价格    品质    类型    销量")
-print(num1,"    ",name1,"    ",jiage1,"    ",pinzhi1,"    ",lexing1,"    ",xiaoliang1)
-print(num2,"    ",name2,"    ",jiage2,"    ",pinzhi2,"    ",lexing2,"    ",xiaoliang2)
-print(num3,"    ",name3,"    ",jiage3,"    ",pinzhi3,"    ",lexing3,"    ",xiaoliang3)
+print(num1,"    ",name1,"    ",jiage1,"    ",pinzhi1,"    ",leixing1,"    ",xiaoliang1)
+print(num2,"    ",name2,"    ",jiage2,"    ",pinzhi2,"    ",leixing2,"    ",xiaoliang2)
+print(num3,"    ",name3,"    ",jiage3,"    ",pinzhi3,"    ",leixing3,"    ",xiaoliang3)
 print("总价=",jiage1*xiaoliang1+jiage2*xiaoliang2+xiaoliang3*jiage3)
