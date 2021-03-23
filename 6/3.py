aae5572acdd3c092cf4e9236265d171581866d20
484669732 <80316502+484669732@users.noreply.github.com>
2021-03-23 20:19:49 +0800

    Add files via upload

---
+++ 6/3.py
@@ -0,0 +1,32 @@
+bank = {
+        '35163969': {'username': '1',
+                     'password': '1',
+                     'country': '1',
+                     'province': '1',
+                     'street': '1',
+                     'door': '1',
+                     'money': 1,
+                     'bank_name': '中国工商银行昌平区回龙观支行'},
+        '35163968': {'username': '1',
+                 'password': '1',
+                 'country': '1',
+                 'province': '1',
+                 'street': '1',
+                 'door': '1',
+                 'money': 1,
+                 'bank_name': '中国工商银行昌平区回龙观支行'},
+
+        }
+print(bank['35163969']['password'])
+
+
+
+main (){
+
+    if {
+    jahdjksakdh
+         {if {}
+
+    }
+}
+}
